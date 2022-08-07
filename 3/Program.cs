8f1e53998e2f99a159d4f901790f594a250dedea
EgorKrylov <KrylovWork@yandex.ru>
2022-08-07 19:11:55 +0500

    initial commit

---
+++ 3/Program.cs
@@ -0,0 +1,7 @@
+﻿
+Console.Write("Номер дня недели (1-7): ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 6 || number == 7) Console.WriteLine("Выходной день");
+else if (number > 0 && number < 6) Console.WriteLine("Будний день");
+else Console.WriteLine("Число не от 1 до 7");
